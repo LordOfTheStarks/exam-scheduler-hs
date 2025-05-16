551a247b0389d2a716ab3dbb9b0b2c0fa58da4e4
Boray Celik <boray.06@hotmail.com>
2025-05-16 10:10:35 +0200

    Exam Model v1.01

--- src/main/java/com/hochschule/exam_scheduler/exam/model/Exam.java
+++ src/main/java/com/hochschule/exam_scheduler/exam/model/Exam.java
@@ -6,106 +6,42 @@ import lombok.Data;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+@Data
 @Entity
-@Table(name = "wise24-25")
+@Table(name = "sose2025")
 public class Exam implements Serializable {
     @Id
-    @Column(name = "course")
-    private String course;
-    private String course_title;
+    @Column(name = "lecture")
+    private String lecture;
+    private String lecture_title;
+    @Id
+    @Column(name = "facultyID")
+    private String facultyId;
+    private String program;
     private String exam_type;
     private String campus;
     private String building;
     private String room;
     private LocalDateTime exam_start_time;
     private int exam_minutes;
+    private String instructor;
 
-    public Exam(String course, String course_title, String exam_type,
-                String campus, String building, String room, LocalDateTime exam_start_time, int exam_minutes) {
-        this.course = course;
-        this.course_title = course_title;
+    public Exam(String lecture, String facultyId, String program, String lecture_title, String exam_type,
+                String campus, String building, String room, LocalDateTime exam_start_time,
+                int exam_minutes, String instructor) {
+        this.lecture = lecture;
+        this.lecture_title = lecture_title;
         this.exam_type = exam_type;
         this.campus = campus;
         this.building = building;
         this.room = room;
+        this.instructor = instructor;
         this.exam_start_time = exam_start_time;
         this.exam_minutes = exam_minutes;
+        this.facultyId = facultyId;
+        this.program = program;
     }
 
-    public String getCourse() {
-        return course;
-    }
-
-    public String getCourse_title() {
-        return course_title;
-    }
-
-    public String getExam_type() {
-        return exam_type;
-    }
-
-    public String getCampus() {
-        return campus;
-    }
-
-    public String getBuilding() {
-        return building;
-    }
-
-    public String getRoom() {
-        return room;
-    }
-
-    public void setCourse(String course) {
-        this.course = course;
-    }
-
-    public void setCourse_title(String course_title) {
-        this.course_title = course_title;
-    }
-
-    public void setExam_type(String exam_type) {
-        this.exam_type = exam_type;
-    }
-
-    public void setCampus(String campus) {
-        this.campus = campus;
-    }
-
-    public void setBuilding(String building) {
-        this.building = building;
-    }
-
-    public void setRoom(String room) {
-        this.room = room;
-    }
-
-    public void setExam_start_time(LocalDateTime exam_start_time) {
-        this.exam_start_time = exam_start_time;
-    }
-
-    public void setExam_minutes(int exam_minutes) {
-        this.exam_minutes = exam_minutes;
-    }
-
-    public String toString() {
-        return "Exam{" +
-                "course='" + course + '\'' +
-                ", course_title='" + course_title + '\'' +
-                ", exam_type'" + exam_type + '\'' +
-                ", campus'" + campus + '\'' +
-                ", building'" + building + '\'' +
-                ", room'" + room + '\'' +
-                ", exam_start_time=" + exam_start_time +
-                ", exam_minutes" + exam_minutes + '\'' +
-                '}';
-    }
-
-    public LocalDateTime getExam_start_time() {
-        return exam_start_time;
-    }
-
-    public int getExam_minutes() {
-        return exam_minutes;
+    public Exam() {
     }
 }
