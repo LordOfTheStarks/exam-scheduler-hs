b70669910a8c91a3532763aa414e8dbd0f22ea85
Boray Celik <boray.06@hotmail.com>
2025-05-16 10:24:47 +0200

    Exam Model + Repo v1.02

--- src/main/java/com/hochschule/exam_scheduler/exam/repository/ExamRepo.java
+++ src/main/java/com/hochschule/exam_scheduler/exam/repository/ExamRepo.java
@@ -1,4 +1,14 @@
 package com.hochschule.exam_scheduler.exam.repository;
 
-public class ExamRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.hochschule.exam_scheduler.exam.model.Exam;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ExamRepo extends JpaRepository<Exam, String> {
+    Optional<Exam> findByFacultyAndProgram(String facultyId, String program);
+
+    void deleteByFacultyAndProgram(String facultyId, String program);
 }
